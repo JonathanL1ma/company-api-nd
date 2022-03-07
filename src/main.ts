a8db5d9df52c54f828a7109b31defa544533a80e
@JonathanL1ma <jonathanalexander.lima@gmail.com>
2022-03-06 22:51:53 -0600

    Property CORS added

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: false });
   app.setGlobalPrefix('api');
   await app.listen(PORT);
 }
