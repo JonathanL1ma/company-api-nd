d56748323d86c995f218e2d27df886a8d2f50e86
@JonathanL1ma <jonathanalexander.lima@gmail.com>
2022-03-06 22:55:45 -0600

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(PORT);
 }
 bootstrap();
